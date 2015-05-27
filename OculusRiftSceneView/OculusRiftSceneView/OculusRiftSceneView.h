710652d6d4d77103f6c030ca28a30c398754a879
Junling Ma <junlingm@gmail.com>
2015-05-27 12:27:20 -0700

    Created a project to put the OculusRiftSceneKitView in a framework name OculusRiftSceneView

---
+++ OculusRiftSceneView/OculusRiftSceneView/OculusRiftSceneView.h
@@ -0,0 +1,16 @@
+//
+//  OculusRiftSceneView.h
+//  OculusRiftSceneView
+//
+//  Created by Junling Ma on 2015-05-27.
+//  Copyright (c) 2015 Junling.Ma. All rights reserved.
+//
+
+#ifndef OculusRiftSceneView_OculusRiftSceneView_h
+#define OculusRiftSceneView_OculusRiftSceneView_h
+
+#import <OculusRiftSceneView/avatar.h>
+#import <OculusRiftSceneView/OculusDevice.h>
+#import <OculusRiftSceneView/OculusRiftSceneKitView.h>
+
+#endif
